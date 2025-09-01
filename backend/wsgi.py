d2d096e3f3705cba0007a5e45385243df2c0f4c9
AP3HIM <thefartz19@gmail.com>
2025-08-31 23:53:11 -0400

    Update wsgi.py with migrate command for Render

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -8,9 +8,16 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
-
+import sys
+import traceback
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
-application = get_wsgi_application()
+try:
+    application = get_wsgi_application()
+except Exception:
+    # Print traceback to browser for debugging
+    def application(environ, start_response):
+        start_response('500 INTERNAL SERVER ERROR', [('Content-Type', 'text/plain')])
+        return [traceback.format_exc().encode('utf-8')]
