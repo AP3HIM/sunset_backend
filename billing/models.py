58e0968d26e045a243092595a5ce9ad894c922e4
AP3HIM <thefartz19@gmail.com>
2025-09-01 01:08:21 -0400

    Update models.py with removed paid plans

--- billing/models.py
+++ billing/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 from django.conf import settings
+from django.contrib.auth.models import User
 
 # Create your models here.
 # billing/models.py
 
 class Customer(models.Model):
-    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     stripe_customer_id = models.CharField(max_length=100)
 
 class Subscription(models.Model):
